1b09581f40a2a2f42ea69559ffc49c547d1f00eb
Nick Ivanov <sm230@pm.me>
2023-04-16 13:08:35 -0400

    Sun 16 Apr 2023 01:08:35 PM EDT

--- start-onefi.sh
+++ start-onefi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd client-daemon && ./start-client.sh
-cd ../provider-daemon && ./start-provider.sh
+#cd client-daemon && ./start-client.sh
+#cd ../provider-daemon && ./start-provider.sh
+cd provider-daemon && ./start-provider.sh
 cd ../app && npm start
