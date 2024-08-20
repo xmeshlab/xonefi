65c2a5ab916ab7972bad1cab5a87acaa97c1e08e
Aditya Chaudhari <98672108+AdityaC4@users.noreply.github.com>
2024-08-20 09:44:15 -0400

    Update start-provider.sh
    fixed roll log 404 issue.

--- provider-daemon/start-provider.sh
+++ provider-daemon/start-provider.sh
@@ -113,6 +113,7 @@ cp ../router/start-vpn.ash /var/www/html/dist
 cp ../router/stop-vpn.ash /var/www/html/dist
 cp ../router/collect-file.ash /var/www/html/dist
 cp ../router/update-router.ash /var/www/html/dist
+cp ../router/roll-log.ash /var/www/html/dist
 cp ../router/get_local_ip.ash /var/www/html/dist
 cp ../router/get_wifi_ssid.ash /var/www/html/dist
 cp /root/xonefi-web/xonefi-app.tar.gz /var/www/html/dist
@@ -144,6 +145,7 @@ chmod 755 /var/www/html/dist/start-vpn.ash
 chmod 755 /var/www/html/dist/stop-vpn.ash
 chmod 755 /var/www/html/dist/collect-file.ash
 chmod 755 /var/www/html/dist/update-router.ash
+chmod 755 /var/www/html/dist/roll-log.ash
 chmod 755 /var/www/html/dist/get_local_ip.ash
 chmod 755 /var/www/html/dist/get_wifi_ssid.ash
 chmod 775 /var/www/html/dist/manage_temp_firewall.ash
